9010c7a96261fc887decd0bb5b0cc5b2e7948514
A Lawrence <97037268+alawre@users.noreply.github.com>
2022-03-23 21:33:46 +0000

    Update Board.js

--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -2,13 +2,36 @@ import React from 'react'
 import Square from './Square'
 
 function Board() {
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialSquares);
+const [xIsNext, setXIsNext] =useState(true);
+
+  const handleClickEvent =  (i) => {
+const newSquares = [...squares];
+
+const winnerDeclared = Boolean (calculateWinner(newSquares));
+const squareFilled = Boolean(newSquares[i]);
+if (winnerDeclared || squareFilled) {
+  return;
+}
+
+newSquares [i] = xIsNext ? 'X' : '0';
+setSquares(newSquares);
+setXIsNext(!xIsNext);
+  };
   const renderSquare = (i) => {
     return (
-      <Square />
-    )
-  }
+      <Square value={squares[i]} 
+      onClickEvent={() => handleClickEvent(i)}
+      />
+    );
+  };
+const winner = calculateWinner(squares);
+  const status = winner ?
+  `Winner: ${winner}`:
+  `Next player: ${xIsNext ? 'X' : 'O'}`;
   return (
-    <div className='board'>Board
+    <div> <div className='status'>{status}</div>
       <div className='board-row'>
       {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
       </div>
@@ -20,5 +43,3 @@ function Board() {
     </div>
   )
 }
-
-export default Board
