3c0ba21c86f705e39c8716c75a52be855ddcf278
A Lawrence <97037268+alawre@users.noreply.github.com>
2022-03-23 21:15:54 +0000

    Update Square.js

--- tictactoe/src/components/Square.js
+++ tictactoe/src/components/Square.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 
 function Square(props) {
-  const [value, setValue] = useState(null)
-
   return (
     <button className='square'
-    onClick={() => setValue('X')}
+    onClick={props.onClickEvent}
     >
-      {value}
+      {props.value}
       
       </button>
   )
 }
- 
+
 export default Square
